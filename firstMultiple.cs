884c0065ebbd35db6113135851dcb545b01b6526
AsuraPham <phamtuananh1202@gmail.com>
2017-11-01 09:22:30 +0700

    Update firstMultiple.cs

--- firstMultiple.cs
+++ firstMultiple.cs
@@ -5,3 +5,19 @@ Example
 
 For divisors = [2, 3, 4] and start = 13, the output should be
 firstMultiple(divisors, start) = 24.
+
+//code
+int firstMultiple(int[] divisors, int start) {
+for (int answer = start;; answer++) {
+			Boolean correct = true;
+			for (int i = 0; i < divisors.Length; i++) {
+				if (answer % divisors[i] != 0) {
+					correct = false;
+					break;
+				}
+			}
+			if (correct) {
+				return answer;
+			}
+		}
+}
